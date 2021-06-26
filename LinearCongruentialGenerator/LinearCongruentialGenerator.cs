2759ddc43dea4ae6b96bd0b0f823c636cfe1fe35
v.piatnytskyi <vitalii.piatnytskyi.pz.2018@lpnu.ua>
2021-06-26 18:12:20 +0300

    Added LinearCongruentialGenerator library.

---
+++ LinearCongruentialGenerator/LinearCongruentialGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinearCongruentialGenerator
+{
+    public class LinearCongruentialGenerator
+    {
+        private int _x;
+        private readonly int _m;
+        private readonly int _a;
+        private readonly int _c;
+
+        public LinearCongruentialGenerator(int seed, int m = int.MaxValue, int a = 48271, int c = 0)
+        {
+            _x = seed != -1 ? seed : (int)(DateTime.Now.Ticks % m);
+
+            _m = m;
+            _a = a;
+
+            _c = c;
+        }
+
+        public int Next()
+        {
+            _x = ((_a * _x) + _c) % _m;
+
+            return _x;
+        }
+    }
+}
