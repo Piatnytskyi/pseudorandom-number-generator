1f614ed450add0b72792baa18c7c53ad6f8ce893
Віталій П'ятницький <Vitalii.Piatnytskyi.PZ.2018@edu.lpnu.ua>
2021-10-01 01:08:39 +0300

    Added infrastructure for async commands.

---
+++ LinearCongruentialGeneratorTest/PeriodSearchProgressEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinearCongruentialGeneratorTest
+{
+    public class PeriodSearchProgressEventArgs : EventArgs
+    {
+        private readonly int _done, _outOf;
+
+        public PeriodSearchProgressEventArgs(int progress, int outOf = 100)
+        {
+            _done = progress;
+            _outOf = outOf;
+        }
+
+        public int Done { get { return _done; } }
+        public int OutOf { get { return _outOf; } }
+    }
+}
